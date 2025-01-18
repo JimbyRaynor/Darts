bacc1e8c37147447e27053072f76b053eb84427e
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-01-19 10:23:23 +1100

    Add files via upload

--- Darts.cpp
+++ Darts.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <ctime>
 #include <chrono> // need for time (and so random) function
+#include <thread> // for pausing
+#include <unistd.h>  // need for getch()
+#include <termios.h>  // need for getch()
 
 using namespace std;
 
@@ -17,10 +20,62 @@ long long seed=0, seed1, seed2; // seed for random numbers
 double p1, p2, p3, p4; // probability waitings for successful shots
 double u; // 0 <= u <= 1,  random number to compare with probability waitings
 
+// this linux function will read a character from the keyboard (<Enter> not required)
+char rbgetchar() {    // watch out for offical getchar() function, it requires  <Enter> !!!
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0) {
+        perror("tcgetattr");
+    }
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0) {
+        perror("tcsetattr");
+    }
+    if (read(0, &buf, 1) < 0) {
+        perror("read");
+    }
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0) {
+        perror("tcsetattr");
+    }
+    return buf;
+}
+
+
+void pause(int t)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(t));
+}
+
 void clearscreen()
 {
  cout << "\033[2J";  
 }
+
+void setblue()
+{
+    cout << "\033[1;38;2;80;80;255m";
+}
+
+void setred()
+{
+    cout << "\033[1;38;2;255;0;0m";
+}
+
+void setyellow()
+{
+    cout << "\033[1;38;2;255;255;0m";
+}
+
+void setgreen()
+{
+    cout << "\033[1;38;2;0;255;0m";
+}
+
 
 void gotoxy(int x, int y)
 {
@@ -55,34 +110,46 @@ void drawrect(int x1, int y1, int x2,int y2)
 
 int main()
 {
+    char inputc;
     clearscreen();
-    drawrect(1,1,70,11);
     seed = chrono::high_resolution_clock::now().time_since_epoch().count();  // better seed for random numbers since it uses nanoseconds
     srand(seed); // set random seed
     s = 0;  // total score
+    setblue();
     gotoxy(3,2 ); cout << "Bullseye ... by David Ahl of Creative Computing";
     gotoxy(3,3 ); cout << "Throw darts at a target with 10, 20, 30 and 40 point zones";
     gotoxy(3,4 ); cout << "First player to reach 200 points wins";
-    drawh(1,5,70);
     gotoxy(3,6 ); cout << "Throw           Description             Probable Score";
-    drawh(1,7,70);
     gotoxy(3,8 ); cout << "  1             Fast Overarm            Bullseye or Complete Miss";
     gotoxy(3,9 ); cout << "  2             Controlled Overarm      10, 20, or 30 points";
     gotoxy(3,10); cout << "  3             Underarm                Anything";
-    drawv(11,6,11); drawv(40,6,11);
+    setgreen();
+    drawrect(1,1,70,11);
+    drawh(1,5,70);
+    drawh(1,7,70);
+    drawv(11,6,11); 
+    drawv(40,6,11);
     r = 0;
     while (s < 200)
     {
         r = r + 1;
         gotoxy(3,16);cout << "Round " << r;
-        gotoxy(3,17);cout << "Input your choice of type of throw (1,2,3) followed by <Enter>: ";
-        cin >> t;
+        gotoxy(3,17);cout << "Input your choice of type of throw (1,2,3):";
+        cout << flush; // draw to screen immediately, do not buffer
+        inputc = rbgetchar();
+        t = (int) inputc - 48;
+        setyellow();
+        for( int i = 0; i<50;i++)
+        {
+         gotoxy(3+i,18);cout << "->" << t;
+         cout << flush; // draw to screen immediately, do not buffer
+         pause(30);
+        }
         switch (t)
         {
         case 1:
-            gotoxy(3,18);cout << "Fast Overarm";
             p1 = 0.65;
-            p2 = 0.55;
+            p2 = 0.5;
             p3 = 0.5;
             p4 = 0.5;
             break;
@@ -93,37 +160,37 @@ int main()
             p4 = 0.01;
             break;
         default:
-            gotoxy(3,18);cout << "default";
             p1 = 0.95;
             p2 = 0.75;
             p3 = 0.45;
             p4 = 0.05;
         }
-        gotoxy(3,22);
         u = 1.0 * rand() / RAND_MAX;
+        gotoxy(54,18);
+        setred();
         if (u >= p1)
         {
-            cout << "Bullseye!! 40 points!";
+            cout << "**Bullseye - 40 points**";
             b = 40;
         }
         else if (u >= p2)
         {
-            cout << "30 points!";
+            cout << "**30 points**";
             b = 30;
         }
         else if (u >= p3)
         {
-            cout << "20 points!";
+            cout << "**20 points**";
             b = 20;
         }
         else if (u >= p4)
         {
-            cout << "10 points!";
+            cout << "**10 points**";
             b = 10;
         }
         else
         {
-            cout << "Whoops! You missed the target!";
+            cout << "**Miss**";
             b = 0;
         }
         s = s + b;
@@ -132,14 +199,3 @@ int main()
     cout << "You finished on Round " << r << "\n";
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
