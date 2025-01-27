85e014cefaa069ef8388eafbb1d172f91d9180af
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-01-27 12:56:59 +1100

    Add files via upload

--- Darts.cpp
+++ Darts.cpp
@@ -120,6 +120,11 @@ void setbackgrey()
     cout << "\033[1;48;2;100;100;100m";
 }
 
+void setbackgrey2()
+{
+    cout << "\033[1;48;2;90;90;90m";
+}
+
 void setyellowongreen()
 {   
     setbackgreen();
@@ -144,10 +149,22 @@ void setyellowonblue()
     setbackblue();   
 }
 
+void setwhiteonblue()
+{
+    setwhite();
+    setbackblue();   
+}
+
 void setwhiteongrey()
 {
     setwhite();
     setbackgrey();   
+}
+
+void setwhiteongrey2()
+{
+    setwhite();
+    setbackgrey2();   
 }
 
 void gotoxy(int x, int y)
@@ -157,34 +174,35 @@ void gotoxy(int x, int y)
 
 void drawh(int x1,int y1,int x2)
 {
-    setbackyellow();
     for (int i = x1;i<=x2;i++)
     {
         gotoxy(i,y1);
         cout << " ";
     }
-    resetmode();
 }
 
 void drawv(int x1,int y1,int y2)
 {
-    setbackyellow();
     for (int i = y1;i<=y2;i++)
     {
         gotoxy(x1,i);
         cout << " ";
     }
-    resetmode();
 }
 
 void drawrect(int x1, int y1, int x2,int y2)
 {
-    
     drawh(x1,y1,x2);
     drawh(x1,y2,x2);
     drawv(x1,y1,y2);
     drawv(x2,y1,y2);
 
+}
+
+void fillrect(int x1, int y1, int x2, int y2)
+{
+   for (int i = y1;i<= y2;i++)
+      drawh(x1,i,x2); 
 }
 
 int calcprobs(char choice, int playernum)
@@ -211,31 +229,30 @@ switch (choice)
             p4 = 0.05;
         }
         u = 1.0 * rand() / RAND_MAX;
-        //gotoxy(57,17+playernum);
         setred();
         if (u >= p1)
         {
-            cout << "**Bullseye - 40 points**";
+            cout << " Bullseye 40";
             b = 40;
         }
         else if (u >= p2)
         {
-            cout << "**30 points**           ";
+            cout << " 30         ";
             b = 30;
         }
         else if (u >= p3)
         {
-            cout << "**20 points**           ";
+            cout << " 20         ";
             b = 20;
         }
         else if (u >= p4)
         {
-            cout << "**10 points**           ";
+            cout << " 10         ";
             b = 10;
         }
         else
         {
-            cout << "**Miss**                ";
+            cout << " Miss       ";
             b = 0;
         }
     cout << flush;
@@ -245,9 +262,9 @@ switch (choice)
 void shoot(char choice, int playernum)
 {
         setyellow();
-        for( int i = 0; i<19;i++)
+        for( int i = 0; i<31;i++)
         {
-         gotoxy(3+i,10+playernum);
+         gotoxy(1+i,8+playernum);
          cout << " -" << choice << "->";
          cout << flush; // draw to screen immediately, do not buffer
          pause(30);
@@ -261,26 +278,29 @@ int main()
     seed = chrono::high_resolution_clock::now().time_since_epoch().count();  // better seed for random numbers since it uses nanoseconds
     srand(seed); // set random seed
     setblueonyellow();
-    gotoxy(3,2 ); cout << "Bullseye ... by David Ahl of Creative Computing                  ";
-    gotoxy(3,3 ); cout << "Throw darts at a target with 10, 20, 30 and 40 point zones       ";
-    gotoxy(3,4 ); cout << "First player to reach 200 points wins                            ";
+    gotoxy(1,2 ); cout << "Bullseye ... by David Ahl of Creative Computing                  ";
+    gotoxy(1,3 ); cout << "Throw darts at a target with 10, 20, 30 and 40 point zones       ";
+    gotoxy(1,4 ); cout << "First player to reach 200 points wins                            ";
     setyellowongreen();
-    gotoxy(3,5 ); cout << "Throw           Description             Probable Score           ";
-    gotoxy(3,6 ); cout << "  1             Fast Overarm            Bullseye or Complete Miss";
-    gotoxy(3,7 ); cout << "  2             Controlled Overarm      10, 20, or 30 points     ";
-    gotoxy(3,8 ); cout << "  3             Underarm                Anything                 ";
-    setgreen();
+    gotoxy(1,5 ); cout << "Throw           Description             Probable Score           ";
     setwhiteongrey();
-    gotoxy(50,11); cout << "Human:           ";
-    gotoxy(50,12); cout << "Computer 1:      ";
-    gotoxy(50,13); cout << "Computer 2:      ";
-    gotoxy(50,14); cout << "Computer 3:      ";
+    gotoxy(1,6 ); cout << "  1             Fast Overarm            Bullseye or Complete Miss";
+    gotoxy(1,7 ); cout << "  2             Controlled Overarm      10, 20, or 30 points     ";
+    gotoxy(1,8 ); cout << "  3             Underarm                Anything                 ";
+    setgreen();
+    setwhiteonblue();
+    gotoxy(48,9); cout <<  "Human:            ";
+    gotoxy(48,10); cout << "Computer 1:       ";
+    gotoxy(48,11); cout << "Computer 2:       ";
+    gotoxy(48,12); cout << "Computer 3:       ";
     r = 0;
     while ( (s < 200) && (s1 < 200) && (s2 < 200) && (s3 < 200) )
     {
+        setbackgreen();
+        fillrect(1,9,47,12);
         r = r + 1;  
         setyellowonblue();
-        gotoxy(3,10);cout << "Round "<< r << ". ";cout << "Input type of throw (1,2,3):";
+        gotoxy(1,13);cout << "Round "<< r << ". ";cout << "Input type of throw (1,2,3):";
         resetmode();
         setblackonred();
         cout << flush; // draw to screen immediately, do not buffer
@@ -288,19 +308,19 @@ int main()
         shoot(choice,1);
         b = calcprobs(choice,1);
         s = s + b;
-        gotoxy(63,11); cout << s;         pause(1000);
+        gotoxy(63,9); cout << s;  gotoxy(40,13); cout << flush;  pause(1000);
         shoot('1',2);
         b = calcprobs('1',2);
         s1 = s1 + b;
-        gotoxy(63,12); cout << s1;         pause(1000);
+        gotoxy(63,10); cout << s1;  gotoxy(40,13); cout  << flush;        pause(1000);
         shoot('2',3);
         b = calcprobs('2',3);
         s2 = s2 + b;
-        gotoxy(63,13); cout << s2;         pause(1000);
+        gotoxy(63,11); cout << s2; gotoxy(40,13); cout  << flush;         pause(1000);
         shoot('3',4);
         b = calcprobs('3',4);
         s3 = s3 + b;
-        gotoxy(63,14); cout << s3;         pause(1000);
+        gotoxy(63,12); cout << s3;  gotoxy(40,13); cout  << flush;        pause(1000);
         resetmode();
     }
     if (s >= 200)
